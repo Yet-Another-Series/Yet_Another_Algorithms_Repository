2ed27651ea4bda97742c5c19c9777f5d9c756647
Rennan Rocha <rennanfreitas11@gmail.com>
2019-10-10 23:53:40 -0300

    Renamed macro

--- Data_Structures/trees/SegmentTree.cpp
+++ Data_Structures/trees/SegmentTree.cpp
@@ -2,8 +2,8 @@
 
 #define ll long long
 
-#define esq(x) 2*(x)
-#define dir(x) (2*(x))+1
+#define left(x) 2*(x)
+#define right(x) (2*(x))+1
 
 #define MAX 1000000
 
@@ -20,8 +20,8 @@ ll arr[MAX];
 ll build(int ind, int l, int r) {
     if(l == r) return segTree[ind] = arr[l-1];
     int mid = (l + r) >> 1;
-    return segTree[ind] = build(esq(ind), l, mid) +
-            build(dir(ind), mid + 1, r);
+    return segTree[ind] = build(left(ind), l, mid) +
+            build(right(ind), mid + 1, r);
 }
 
 /**
@@ -36,8 +36,8 @@ ll query(int ind, int l, int r, int A, int B) {
     if(l >= A && r <= B) return segTree[ind];
     if(r < A || l > B) return 0;
     int mid = (l + r) >> 1;
-    return query(esq(ind), l, mid, A, B) +
-            query(dir(ind), mid+1, r, A, B);
+    return query(left(ind), l, mid, A, B) +
+            query(right(ind), mid+1, r, A, B);
 }
 
 /**
@@ -51,9 +51,9 @@ ll query(int ind, int l, int r, int A, int B) {
 ll update(int ind, int l, int r, int x, int val) {
     if(l == r) return segTree[ind] += val;
     int mid = (l + r) >> 1;
-    if(x <= mid) update(esq(ind), l, mid, x, val);
-    else update(dir(ind), mid+1, r, x, val);
-    return segTree[ind] = segTree[esq(ind)] + segTree[dir(ind)];
+    if(x <= mid) update(left(ind), l, mid, x, val);
+    else update(right(ind), mid+1, r, x, val);
+    return segTree[ind] = segTree[left(ind)] + segTree[right(ind)];
 }
 
 int main() {
