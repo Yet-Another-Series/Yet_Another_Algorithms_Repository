f39294040bb434f2919e8fc8e863755763af6c1c
Jasani Dhruv <jd_rock786@yahoo.com>
2019-10-14 20:04:26 +0530

    ShellSort Algo Added :)

---
+++ Algorithms/Sorting/ShellSort.cpp
@@ -0,0 +1,40 @@
+//Shell Sort by Jasani Dhruv
+#include<iostream> 
+using namespace std;
+ 
+void sort(int a[],int n)
+{
+	int gap,i,j,temp;
+ 
+	for(gap=n/2;gap>0;gap/=2)
+	{
+		for(i=gap;i<n;i+=1)
+		{
+			temp=a[i];
+			for(j=i;j>=gap&&a[j-gap]>temp;j-=gap)
+				a[j]=a[j-gap];
+			
+			a[j]=temp;
+		}
+    }
+}
+ 
+int main()
+{
+	int a[20],i,n;
+	cout<<"Shell Sort Algoritm"<<endl;
+	cout<<"Enter number of elements:";
+	cin>>n;
+		
+	cout<<"Enter array elements:\n";
+	for(i=0;i<n;++i)
+		cin>>a[i];
+ 
+	sort(a,n);
+ 
+	cout<<"\nArray after shell sort:\n";
+	for(i=0;i<n;++i)
+		cout<<a[i]<<" ";
+ 
+    return 0;
+}
