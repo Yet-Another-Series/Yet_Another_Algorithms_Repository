dbae4d81d4613863cf75bb348c25584d5586838f
Nanubala Gnana Sai <45007169+NanuSai@users.noreply.github.com>
2019-10-15 21:11:09 +0530

    Add files via upload

---
+++ Algorithms/factorial/cpp-factorial-O(n)-1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>  
+  
+using namespace std; 
+  
+int fibonnaci(int n){
+
+    int f[n+2];
+
+
+    f[0] = 0;
+    f[1] = 1;
+
+    for(int i =2 ; i <= n ; i++){
+        f[i] = f[i-1] + f[i-2];
+    }
+
+    return f[n];
+
+}
+
+int main() 
+{   
+    int n;
+    cout << "Enter your number: ";
+
+    cin >> n;
+
+    long int fib = fibonnaci(n);
+
+    cout << fib << endl;
+
+
+
+
+      
+    return 0; 
+} 
