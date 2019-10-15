e165538933832dfe04bd61db150432dc818e329b
Kyle Miller <frgttnsin@gmail.com>
2019-10-15 13:22:43 -0600

    implement bubble sort

--- Algorithms/bubble_sort/python-bubble_sort-O(n*n).py
+++ Algorithms/bubble_sort/python-bubble_sort-O(n*n).py
@@ -1,1 +1,18 @@
+def bubble_sort(list): # sort in place and return it
+    l = len(list)
 
+    for i in range(l):
+        swapped_el = False
+
+        for j in range(0, l-i-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+                swapped_el = True
+
+        if not swapped_el:
+            break
+    return list
+
+list = input('Enter a list of numbers separated by spaces: ').split()  
+list = [int(x) for x in list]
+print('Sorted list: {}'.format(bubble_sort(list)))
