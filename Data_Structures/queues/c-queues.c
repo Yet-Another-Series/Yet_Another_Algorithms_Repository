1c59106804ee5da5b23a2fe9fb42a21a49fa8eb5
idruk <alexandre.sananikone@epitech.eu>
2019-10-15 21:36:41 +0100

    add documentation

--- Data_Structures/queues/c-queues.c
+++ Data_Structures/queues/c-queues.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/** Data structure I use in this (data type can be replace by another type)
+ */
 typedef struct s_queue
 {
     int data;
     struct s_queue *next;
 } t_queue;
 
+/** typedef for bool type in C
+ */
 typedef enum { false, true } bool;
 
+/** front: return the first data of the list
+ * @param reference of the head node
+ * @return data from the front node
+ */
 int front(t_queue *head)
 {
     return (head->data);
 }
-
+/** back: return the last data of the list
+ * @param reference of the head node
+ * @return data from the last node
+ */
 int back(t_queue *head)
 {
     while (head->next != NULL) {
@@ -22,6 +33,9 @@ int back(t_queue *head)
     return (head->data);
 }
 
+/** empty: return true if the queue is empty and false otherwise
+ * @param reference of the head node
+ */
 bool empty(t_queue *head)
 {
     if (head == NULL)
@@ -29,6 +43,22 @@ bool empty(t_queue *head)
     return (false);
 }
 
+/** size: return the size of the queue
+ * @param reference of the head node
+ * @return size of the queue
+ */
+unsigned int size(t_queue *head)
+{
+    int size;
+
+    for (size = 0; head != NULL; size++, head = head->next);
+    return (size);
+}
+
+/** push: the data in the queue
+ * @param reference of the head node
+ * @param data you want to push in the queue
+ */
 void push(t_queue **head, int data)
 {
     t_queue *new = malloc(sizeof(t_queue));
@@ -39,6 +69,10 @@ void push(t_queue **head, int data)
     (*head) = new;
 }
 
+/** emplace: put the data a the end of the queue
+ * @param reference of the head node
+ * @param data you want to emplace in the queue
+ */
 void emplace(t_queue **head, int data)
 {
     t_queue *new = malloc(sizeof(t_queue));
@@ -57,6 +91,9 @@ void emplace(t_queue **head, int data)
     tmp->next = new;
 }
 
+/** pop: pop the first value of the queue
+ * @param reference of the head node
+ */
 void pop(t_queue **head)
 {
     t_queue *new = *head;
@@ -65,6 +102,9 @@ void pop(t_queue **head)
     free(new);
 }
 
+/** display_list: display the list for debug
+ *  @param reference of the head node
+ */
 void display_list(t_queue *head)
 {
     printf("Displaying:\n");
@@ -74,14 +114,17 @@ void display_list(t_queue *head)
     }
 }
 
-int main()
-{
-    t_queue *test = NULL;
-    emplace(&test, 69);
-    // pop(&test);
-    push(&test, 1);
-    push(&test, 5);
-    printf("%d\n", back(test));
-    display_list(test);
-    return (0);
-}
+
+/**
+*/
+// int main()
+// {
+//     t_queue *test = NULL;
+//     emplace(&test, 69);
+//     pop(&test);
+//     push(&test, 1);
+//     push(&test, 5);
+//     printf("%d\n", size(test));
+//     display_list(test);
+//     return (0);
+// }
