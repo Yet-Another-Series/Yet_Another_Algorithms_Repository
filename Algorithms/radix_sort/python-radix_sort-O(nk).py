d51a375a615c90d2dd80e7aa07df86058f52f590
niharika412 <37020410+niharika412@users.noreply.github.com>
2019-10-21 00:24:30 +0530

    python-radix_sort-O(nk).py

---
+++ Algorithms/radix_sort/python-radix_sort-O(nk).py
@@ -0,0 +1,32 @@
+#RADIX SORT
+def radixsort( aList ):
+  RADIX = 10
+  maxLength = False
+  tmp , placement = -1, 1
+ 
+  while not maxLength:
+    maxLength = True
+    # declare and initialize buckets
+    buckets = [list() for _ in range( RADIX )]
+ 
+    # split aList between lists
+    for  i in aList:
+      tmp = i //placement
+      buckets[tmp % RADIX].append( i )
+      if maxLength and tmp > 0:
+        maxLength = False
+ 
+    # empty lists into aList array
+    a = 0
+    for b in range( RADIX ):
+      buck = buckets[b]
+      for i in buck:
+        aList[a] = i
+        a += 1
+ 
+    # move to next digit
+    placement *= RADIX
+  return aList 
+
+arr = [ 12, 34, 54, 2, 3] 
+print(*radixsort(arr))
