3e8f18e63255ff71f401eefa37a0530e6fd4504a
agarwal-123 <43489651+agarwal-123@users.noreply.github.com>
2019-10-21 15:45:22 -0400

    Update cpp-binary_search-O(log_n).cpp
    Adding comments and making the code understanding.

--- Algorithms/binary_search/cpp-binary_search-O(log_n).cpp
+++ Algorithms/binary_search/cpp-binary_search-O(log_n).cpp
@@ -19,9 +19,9 @@ int main()
 	while(l<h)
 	{
 		mid=(l+h)/2;
-		if(a[mid]>n) {h=mid-1;}
-		else if(a[mid]<n) {l=mid+1;}
-		else {flag=1; break;}
+		if(a[mid]>n) {h=mid-1;}		//If element is smaller than a[mid] then it mush be present at left of mid 
+		else if(a[mid]<n) {l=mid+1;}	//If element is greater than a[mid] then it must be present at right of mid
+		else {flag=1; break;}		//If element is found at mid
 	}
 	if(flag==1)
         cout<<mid;
