2c263a4d45c3e10fe40717216b108cfac90ba73b
jsenior10 <jacobsenior4@gmail.com>
2019-10-22 22:21:53 +0100

    Added C++ implementation of radix sort

--- Algorithms/radix_sort/cpp-radix_sort-O(n(log(n)).cpp
+++ Algorithms/radix_sort/cpp-radix_sort-O(n(log(n)).cpp
@@ -1,7 +1,10 @@
 #include <iterator>
 #include <algorithm>
 #include <vector>
+#include <iostream>
 
+//templates used to allow most containers to be used, as long as they contain only integral types 
+// (short, long, int, etc)
 template <typename RandomIterator>
 void radixSort(RandomIterator first, RandomIterator last, int bucket_count)
 {
@@ -12,12 +15,12 @@ void radixSort(RandomIterator first, RandomIterator last, int bucket_count)
 	static_assert(std::is_integral<ValueType>::value,
 		"container can only have integral types ");
 
-	ValueType max_value = *(std::max_element(first, last));
+	ValueType maxValue = *(std::max_element(first, last));
 	ValueType div{};
 	std::vector<std::vector<ValueType>> buckets(bucket_count);
 
 	ValueType i = 1;
-	while (max_value != 0)
+	while (maxValue != 0)
 	{
 		// move elements into the correct buckets
 		for (RandomIterator iterator = first; iterator != last; ++iterator)
@@ -42,19 +45,50 @@ void radixSort(RandomIterator first, RandomIterator last, int bucket_count)
 			bucket.clear();
 		}
 
-		max_value /= 10;
+		maxValue /= 10;
 		i *= 10;
 	}
 }
 
-/**
- * \brief Use this function for implementation, the function is overloaded and will use the correct function definition based on the parameters passed.
- * \tparam IntegralTypeContainer Template that will handle most (hopefully) containers as long as they contain only integral types.
- * \param container The container that contains your list of elements.
- * \param bucket_count The number of buckets you would like to use.
- */
+/// <param name="container">The container that contains your list of elements of INTEGRAL TYPE. Can be short, long, int, etc.</param>
+/// 
+/// Use this function for implementation, the function is overloaded and will use the 
+/// correct function definition based on the parameters passed
 template <typename IntegralTypeContainer>
 void radixSort(IntegralTypeContainer& container, int bucket_count)
 {
 	radixSort(std::begin(container), std::end(container), bucket_count);
+}
+
+template <typename IntegralTypeContainer>
+void generateRandomVector(IntegralTypeContainer& input_array, int array_size) {
+	for (size_t i = 0; i < array_size; ++i) {
+		input_array.push_back(rand() % 10000);
+	}
+}
+
+template <typename IntegralTypeContainer>
+void copyVector(std::vector<IntegralTypeContainer>& array_to_copy, std::vector<IntegralTypeContainer>& array_to_copy_to)
+{
+	for (auto elem : array_to_copy)
+	{
+		array_to_copy_to.push_back(elem);
+	}
+}
+
+
+
+int main()
+{
+	std::vector<unsigned long> unsorted_array;
+	std::vector<unsigned long> pre_sorted_array;
+	generateRandomVector(unsorted_array, 10);
+	copyVector(unsorted_array, pre_sorted_array);
+	std::sort(pre_sorted_array.begin(), pre_sorted_array.end());
+	radixSort(unsorted_array, 10);
+
+	const bool test_passed = (unsorted_array == pre_sorted_array);
+	std::cout << "test " << (test_passed ? "passed\n" : "failed\n");
+
+	return 0;
 }
