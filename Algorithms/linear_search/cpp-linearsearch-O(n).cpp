afdc9ddc235cb9085c4c8ff69ef285b53eb782cc
Gelonit <49699042+Gelonit@users.noreply.github.com>
2019-10-23 20:05:11 +0300

    Created cpp-linearsearch-O(n).cpp

--- Algorithms/linear_search/cpp-linearsearch-O(n).cpp
+++ Algorithms/linear_search/cpp-linearsearch-O(n).cpp
@@ -1,13 +1,19 @@
-#ifndef LINEAR_H
-#define LINEAR_H
-    
-    // Function searches for an int in an array and returns its position, or -1 if it's not found.
-    
-    int get_pos(int array[], int target){
-         for(unsigned int i = 0; i < (sizeof(array) / sizeof(*array)); i++){
-              if(array[i] == target) return i;
+#include <iostream>                                                                    
+using namespace std;
+
+int main(){
+     int array[] = {1,2,13,4,5,6};
+     int target = 3;
+     bool found = false;
+     unsigned int i;
+      
+     for(i = 0; i < (sizeof(array) / sizeof(array[0])); i++){
+         if(array[i] == target){
+               cout << i << endl;
+               found = true;
          }
-         return -1;
-    }
-    
-#endif
+     }
+     if(!found) cout << -1 << endl;
+            
+     return 0;
+}
