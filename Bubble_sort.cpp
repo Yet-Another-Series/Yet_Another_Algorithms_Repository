722e9d0e2485fde8f773ce8f6073eb807a4d43d5
Rujhan Arora <rujhanarora.96@gmail.com>
2019-10-26 01:21:39 +0530

    Swap without using extra variable

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
 using namespace std;
 void swapping(int &a, int &b) {      //swap the content of a and b
-   int temp;
-   temp = a;
-   a = b;
-   b = temp;
+   a = a + b;
+   b = a - b;
+   a = a - b;
 }
 void display(int *array, int size) {
    for(int i = 0; i<size; i++)
