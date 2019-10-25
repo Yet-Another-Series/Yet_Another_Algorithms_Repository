68d5cca028fa69262e8d442d7b731566dd8c669c
Ajay Kumar <ajaymeena3210@gmail.com>
2019-10-25 19:09:06 +0530

    binary search added

--- Algorithms/binary_search/binary_search.py
+++ Algorithms/binary_search/binary_search.py
@@ -7,10 +7,7 @@ def bubble_sort(arr):
 
         # Last i elements are already in place
         for j in range(0, n - i - 1):
-
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
+            
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return(arr)
