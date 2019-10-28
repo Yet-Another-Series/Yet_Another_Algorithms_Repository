de6ee5a841b46924c83ff8ba938b2fb0038f6e4e
Nisha S <nishasuresh2001@gmail.com>
2019-10-28 10:24:00 +0530

    linear search

--- Algorithms/linear_search/cpp-linear_search-O(n).cpp
+++ Algorithms/linear_search/cpp-linear_search-O(n).cpp
@@ -1,24 +1,32 @@
 #include <bits/stdc++.h>
+#include <iostream.h>
 using namespace std;
 
+int linear_search(int arr[], int len,int key)
+{
+	for(int i=0;i<len;i++)
+	{
+		if(arr[i]==key)
+		{
+			return i+1;
+		}
+	}
+	return -1;
+}	
 int main()
 {
 	int n,key;
 	cin>>n>>key;
 	int A[n];
+	cout<<"Enter the elements : "<<endl;
 	for (int i = 0; i < n; ++i)
 	{
 		cin>>A[i];
 	}
-	int i;
-	for (i = 0; i < n; ++i)
-	{
-		if(A[i]==key)
-			break;
-	}
-	if(i==n)
-		cout<<"Not Found"<<endl;
+	int res=linear_search(A,n,key)
+	if(res!=-1)
+		cout<<"Found at position "<<res;
 	else
-		cout<<"Found"<<endl;
-	
+		cout<<"Not Found"<<endl;
+	return 0;
 }
