976074ae6a5fe45d129869b221ae5d8f912724a3
bhanuprakash299 <bhanu26092815@gmail.com>
2019-10-28 13:38:41 +0530

    code for quick sort

--- Algorithms/Quick_Sort.cpp
+++ Algorithms/Quick_Sort.cpp
@@ -1,62 +1,38 @@
-// CPP code for recursive function of Quicksort 
-#include <bits/stdc++.h> 
-
-using namespace std; 
-
-// Function to swap numbers 
-void swap(int* a, int* b) 
-{ 
-	int temp = *a; 
-	*a = *b; 
-	*b = temp; 
-} 
-
-/* This function takes last element as pivot, 
-places the pivot element at its correct 
-position in sorted array, and places 
-all smaller (smaller than pivot) to left 
-of pivot and all greater elements to 
-right of pivot */
-int partition(int arr[], int l, int h) 
-{ 
-	int x = arr[h]; 
-	int i = (l - 1); 
-
-	for (int j = l; j <= h - 1; j++) { 
-		if (arr[j] <= x) { 
-			i++; 
-			swap(&arr[i], &arr[j]); 
-		} 
-	} 
-	swap(&arr[i + 1], &arr[h]); 
-	return (i + 1); 
-} 
-
-/* A[] --> Array to be sorted, 
-l --> Starting index, 
-h --> Ending index */
-void quickSort(int A[], int l, int h) 
-{ 
-	if (l < h) { 
-		/* Partitioning index */
-		int p = partition(A, l, h); 
-		quickSort(A, l, p - 1); 
-		quickSort(A, p + 1, h); 
-	} 
-} 
-
-// Driver code 
-int main() 
-{ 
-
-	int n = 5; 
-	int arr[n] = { 4, 2, 6, 9, 2 }; 
-
-	quickSort(arr, 0, n - 1); 
-
-	for (int i = 0; i < n; i++) { 
-		cout << arr[i] << " "; 
-	} 
-
-	return 0; 
-} 
+#include<stdio.h>
+void quicksort(int number[25],int first,int last){
+int i, j, pivot, temp;
+if(first<last){
+pivot=first;
+i=first;
+j=last;
+while(i<j){
+while(number[i]<=number[pivot]&&i<last)
+i++;
+while(number[j]>number[pivot])
+j--;
+if(i<j){
+temp=number[i];
+number[i]=number[j];
+number[j]=temp;
+}
+}
+temp=number[pivot];
+number[pivot]=number[j];
+number[j]=temp;
+quicksort(number,first,j-1);
+quicksort(number,j+1,last);
+}
+}
+int main(){
+int i, count, number[25];
+printf("Enter some elements (Max. - 25): ");
+scanf("%d",&count);
+printf("Enter %d elements: ", count);
+for(i=0;i<count;i++)
+scanf("%d",&number[i]);
+quicksort(number,0,count-1);
+printf("The Sorted Order is: ");
+for(i=0;i<count;i++)
+printf(" %d",number[i]);
+return 0;
+}
