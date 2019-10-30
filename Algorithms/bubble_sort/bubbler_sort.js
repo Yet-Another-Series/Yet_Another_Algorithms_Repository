c85463728b6dae6bd3d8a66d635e9944d0612fcf
Samuel Loza <starsaminf@gmail.com>
2019-10-30 10:50:30 -0400

    Create bubble_sort implementation in js

---
+++ Algorithms/bubble_sort/bubbler_sort.js
@@ -0,0 +1,21 @@
+
+const array = [2, 3, 5, 6, 1, 90, 6];
+
+console.log(`Array disordered [${array}]`);
+
+bubbleSort = (array) => {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                let tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+
+        }
+    }
+}
+
+bubbleSort(array)
+
+console.log(`Array Sorted [${array}]`);
